7b1d79166565121c424c1f834f6df4c26858f60f
alvaroahp11 <supercorroncho1@gmail.com>
2020-12-14 10:14:36 +0100

    asd

--- src/cosmos/Cosmos.java
+++ src/cosmos/Cosmos.java
@@ -1,96 +1,110 @@
 package cosmos;
- 
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
-
+import manageStates.*;
 import exceptions.CosmosException;
 
 public class Cosmos {
-	
+
 	public static void checkArguments(String args[]) throws CosmosException {
-		//Only 2 arguments
-		if(args.length != 2) {
+		// Only 2 arguments
+		if (args.length != 2) {
 			throw new CosmosException("\tThe structure must be:\n ./cosmos.java <problema.prob> <heuristica>");
-		}else if(!args[1].equals("heuristica1") && !args[1].equals("heuristica2")) { //La heuristica esta definida
+		} else if (!args[1].equals("heuristica1") && !args[1].equals("heuristica2")) { // La heuristica esta definida
 			throw new CosmosException("La heuristica no esta definida");
 		}
-		
+
 	}
-	
+
 	public static String[] readFile(File f) throws CosmosException {
 		String data[] = new String[3];
 		try {
 			Scanner myReader = new Scanner(f);
-			
-			for(int i=0; myReader.hasNextLine() == true && i<3; i++) {
+
+			for (int i = 0; myReader.hasNextLine() == true && i < 3; i++) {
 				data[i] = myReader.nextLine();
 			}
 			myReader.close();
-		}catch(FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			throw new CosmosException("Cant find the file: " + f.getPath());
-		}		
+		}
 		return data;
 	}
-	
-	public	static void dataFilter(String data[]) throws CosmosException{
-		for (int i = 0; i < data.length; i++){
-			if(data[i].equals(null)){
-				throw new CosmosException("Wrong input format \n\tMust be like: \n" + 
-				                               "OBS: (i,j);(i,j); ... ;(i,j)\r\n" + 
-				                               "SAT1: 1;1;1;1;nbattery\r\n" + 
-				                               "SAT2: 1;1;1;1;nbattery");
+
+	public static String[] dataFilter(String data[]) throws CosmosException {
+
+		for (int i = 0; i < data.length; i++) {
+			if (data[i].equals(null)) {
+				throw new CosmosException("Wrong input format \n\tMust be like: \n" + "OBS: (i,j);(i,j); ... ;(i,j)\r\n"
+						+ "SAT1: 1;1;1;1;nbattery\r\n" + "SAT2: 1;1;1;1;nbattery");
 			}
 			String line[] = data[i].split(":");
-			if(!(line[0].equals("OBS") || line[0].equals("SAT1") || line[0].equals("SAT2"))) {
-				throw new CosmosException("Wrong input format \n\tMust be like: \n" + 
-				                            "OBS: (i,j);(i,j); ... ;(i,j)\r\n" + 
-				                            "SAT1: 1;1;1;1;nbattery\r\n" + 
-				                            "SAT2: 1;1;1;1;nbattery");
+			if (!((line[0].equals("OBS") && i == 0) || (line[0].equals("SAT1") && i == 1)
+					|| (line[0].equals("SAT2") && i == 2))) {
+				throw new CosmosException("Wrong input format \n\tMust be like: \n" + "OBS: (i,j);(i,j); ... ;(i,j)\r\n"
+						+ "SAT1: 1;1;1;1;nbattery\r\n" + "SAT2: 1;1;1;1;nbattery");
 			}
-		}		
+			data[i] = line[1];
+		}
+		return data;
 	}
 
-//	public static State getInfo(String data[]) {
-//	    
-//		
-//	}
+	public static State firstState(String data[]) {
+		State intialState = null;
+		for (int i = 0; i < data.length; i++) {
+			// Eliminamos lo que va antes de lo valores
+			if (i == 0) {
 
-	public static void main (String args[]) throws CosmosException, FileNotFoundException {
-		
-		//Check the arguments
+				String line[] = data[i].split(";");
+				for (int j = 0; j < data.length; j++) {
+					System.out.println(line[j]);
+				}
+
+			} else if (i == 2) {
+
+			} else {
+
+			}
+
+			String linea[] = data[i].split(";");
+		}
+
+		return intialState;
+	}
+
+	public static void main(String args[]) throws CosmosException, FileNotFoundException {
+
+		// Check the arguments
 		checkArguments(args);
-		
-		//Open the file
+
+		// Open the file
 		File input;
 		try {
 			input = new File(args[0]);
-		}catch (NullPointerException e) {
+		} catch (NullPointerException e) {
 			throw new CosmosException("Input File is null");
 		}
-		//Comprobar que el file existe
-		if(!input.exists()) {
+		// Comprobar que el file existe
+		if (!input.exists()) {
 			throw new CosmosException("The file " + input.getPath() + " doesnt exists");
 		}
-		
-		//Data from file
+
+		// Data from file
 		String data[] = readFile(input);
-		
-		//Check syntax of file
+
+		// Check syntax of file
 		try {
-		     dataFilter(data);
-		}catch(CosmosException e) {
-		    throw e;
+			dataFilter(data);
+
+		} catch (CosmosException e) {
+			throw e;
 		}
-		
-		System.out.println(data[0]);
-		System.out.println(data[1]);
-		System.out.println(data[2]);
 
+		firstState(data);
 
-		
 	}
-	
+
 }
--- src/manageStates/State.java
+++ src/manageStates/State.java
@@ -8,16 +8,23 @@ public class State {
 	private Satelite SAT1;
 	private Satelite SAT2;
 	private boolean area [][];
+	private int j;
+
 	
 	private State parent;
 	private int costValue;
 	private int heuristicValue;
 
+
+	//Initial constructor
     public State (Satelite SAT1, Satelite SAT2, boolean area[][]) {
     	this.SAT1 = SAT1;
     	this.SAT2 = SAT2;
     	this.area = area;
+		this.j = 0;
     }
+
+	
     
     
     
@@ -25,7 +32,77 @@ public class State {
         
         return null;
     }
-    
-    
 
+
+	private State sat1NadaSat2Nada (){
+		State chld = this;
+		chld.setParent(this);
+		chld.setCostValue(this.getCostValue() + 1);
+		chld.setJ(this.getJ() + 1);
+
+		return chld;
+	}
+
+
+
+	public boolean isFinal(){
+		return true;
+	}
+	
+
+	public Satelite getSAT1() {
+		return SAT1;
+	}
+
+	public void setSAT1(Satelite sAT1) {
+		SAT1 = sAT1;
+	}
+
+	public Satelite getSAT2() {
+		return SAT2;
+	}
+
+	public void setSAT2(Satelite sAT2) {
+		SAT2 = sAT2;
+	}
+
+	public boolean[][] getArea() {
+		return area;
+	}
+
+	public void setArea(boolean[][] area) {
+		this.area = area;
+	}
+
+	public int getJ() {
+		return j;
+	}
+
+	public void setJ(int j) {
+		this.j = j;
+	}
+
+	public State getParent() {
+		return parent;
+	}
+
+	public void setParent(State parent) {
+		this.parent = parent;
+	}
+
+	public int getCostValue() {
+		return costValue;
+	}
+
+	public void setCostValue(int costValue) {
+		this.costValue = costValue;
+	}
+
+	public int getHeuristicValue() {
+		return heuristicValue;
+	}
+
+	public void setHeuristicValue(int heuristicValue) {
+		this.heuristicValue = heuristicValue;
+	}
 }
